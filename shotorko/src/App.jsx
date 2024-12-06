e768664230ba147a4c633d48a0e1d36547d603ec
abidreal <abidreal9029@gmail.com>
2024-12-06 12:12:48 +0600

    Initial commit on the web branch

---
+++ shotorko/src/App.jsx
@@ -0,0 +1,18 @@
+
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header/Header'
+import Map from './components/Map/Map'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <Header></Header>
+      <Map></Map>
+    </div>
+  )
+}
+
+export default App
---
+++ shotorko/src/components/Map/Map.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Map.css';
+
+
+const Map = () => {
+    return (
+        <map className='map'>
+Map
+            
+        </map>
+    );
+};
+
+export default Map;

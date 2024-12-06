209b4368d17f33e28f816e6666d141e7ec835a3a
abidreal <abidreal9029@gmail.com>
2024-12-06 15:33:25 +0600

    navigation bug fixed

--- shotorko/src/components/Header/Header.jsx
+++ shotorko/src/components/Header/Header.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import './Header.css';
 import logo from '../../assets/logo.jpg';
-
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 const Header = () => {
     return (
         <nav className='header'>
-            <img src={logo} className="w-full h-auto" alt="" />
+            <img src={logo} className="w-full h-auto" alt="Logo" />
             
+            {/* Button to scroll to the map section */}
+            <div className="flex justify-center items-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                <AnchorLink href="#mapSection">
+                    <button className="bg-blue-500 text-white px-6 py-3 rounded-lg hover:bg-blue-600 transition">
+                        Click here to go to map
+                    </button>
+                </AnchorLink>
+            </div>
         </nav>
     );
 };
--- shotorko/src/components/Map/Map.jsx
+++ shotorko/src/components/Map/Map.jsx
@@ -6,12 +6,14 @@ import "leaflet/dist/leaflet.css";
 
 const Map = () => {
     return (
+        <div id="mapSection" className="map-container mt-60 mb-24">
+<div className="w-full h-[500px] mt-16">
+                <Heatmap />
+            </div></div>
 
             
 
-            <div className="w-full h-[500px] mt-16">
-                <Heatmap />
-            </div>
+            
     );
 };
 
